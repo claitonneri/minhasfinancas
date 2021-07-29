e575378c6968c2f116a7692e99da5bfa55e6ebef
claitonneri <claitondidone@gmail.com>
2021-07-29 14:28:27 -0300

    chore: inclusão das categorias

---
+++ src/resources/categories.ts
@@ -0,0 +1,10 @@
+export const categories = [
+  { key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#7a5618' },
+  { key: 'food', name: 'Alimentação', icon: 'coffee', color: '#2D2D2C' },
+  { key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#12A454' },
+  { key: 'car', name: 'Carro', icon: 'crosshair', color: '#E83F5B' },
+  { key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195C' },
+  { key: 'studies', name: 'Estudos', icon: 'book', color: '#9C001A' },
+];
+
+export default categories;
