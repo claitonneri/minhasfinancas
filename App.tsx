322f83d2e36f5ce3e6fdb1845966dd6759d0ed51
claitonneri <claitondidone@gmail.com>
2021-07-29 14:25:22 -0300

    chore: projeto inicial

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+import Routes from './src/routes';
+
+import { AuthProvider, useAuth } from './src/hooks/auth';
+
+import theme from './src/styles/theme';
+
+const App: React.FC = () => {
+  const { userLoading } = useAuth();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded && !userLoading) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="#2D2D2C" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
